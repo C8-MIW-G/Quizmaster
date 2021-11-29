922a0d926e3708ef1317e58f8cd7be6e0804b1cf
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-29 14:30:26 +0100

    import junit5 and Test the UserDAO.storeOne() function

--- src/test/java/database/mysql/UserDAOTest.java
+++ src/test/java/database/mysql/UserDAOTest.java
@@ -1,4 +1,32 @@
+package database.mysql;
+
+import model.User;
+import org.junit.jupiter.api.Test;
+import view.SceneManager;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserDAOTest {
-  
+
+    @Test
+    void storeOne() {
+        // Arrange
+        User user = new User("Vincent", "VincentPW");
+        DBAccess dbAccess = new DBAccess("Quizmaster",
+                "userQuizmaster", "userQuizmasterPW");
+        dbAccess.openConnection();
+        UserDAO userDAO = new UserDAO(dbAccess);
+        int defaultUserId = -1;
+
+        // Activate
+        userDAO.storeOne(user);
+        dbAccess.closeConnection();
+
+        // Assert
+        assertNotEquals(defaultUserId, user.getUserId());
+    }
+
+    @Test
+    void updateOne() {
+    }
 }
