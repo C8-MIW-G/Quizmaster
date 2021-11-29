ba34743c8fa4ffb9d9ade3f03fe0ff98aac39915
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-29 14:21:55 +0100

    added basic functionality to User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,3 +1,42 @@
 package model;
 
-public class User {}
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ *
+ * Generic user that can log in and not much else
+ */
+public class User {
+
+    private int userId = -1; // userId should be set by the database, if it is not set it is -1
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/database/mysql/TestDB.java
@@ -0,0 +1,7 @@
+package database.mysql;/**
+ * @author  Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ *
+ * Dit is wat het programma doet.   
+ */
+public class TestDB {
+}
