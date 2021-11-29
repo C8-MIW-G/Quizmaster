f593905dd5701051e816980dc5d90d35eb5820c7
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-29 14:29:13 +0100

    Java code for writing/updating Users to the DB

--- src/main/java/database/mysql/UserDAO.java
+++ src/main/java/database/mysql/UserDAO.java
@@ -1,7 +1,69 @@
-package database.mysql;/**
- * @author  Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+package database.mysql;
+
+import model.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
  *
- * Dit is wat het programma doet.   
+ * Save and get User objects from the database
  */
-public class UserDAO {
+public class UserDAO extends AbstractDAO implements GenericDAO<User> {
+
+    public UserDAO(DBAccess dbAccess) {
+        super(dbAccess);
+    }
+
+    @Override
+    public ArrayList<User> getAll() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public User getOneById(int id) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void storeOne(User user) {
+        if (user.getUserId() != -1) {
+            updateOne(user);
+            return;
+        }
+
+        String sql = "INSERT INTO User (username, password) VALUES (?, ?);";
+
+        try {
+            setupPreparedStatementWithKey(sql);
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            int userId = executeInsertStatementWithKey();
+            user.setUserId(userId);
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException);
+        }
+    }
+
+    public void updateOne(User user) {
+        if (user.getUserId() == -1) {
+            throw new IllegalArgumentException("User object without id cannot be updated");
+        }
+
+        String sql = "UPDATE User SET (username, password) VALUES (?, ?) WHERE userId = ?;";
+
+        try {
+            setupPreparedStatementWithKey(sql);
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getUserId());
+            int userId = executeInsertStatementWithKey();
+            user.setUserId(userId);
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException);
+        }
+    }
 }
